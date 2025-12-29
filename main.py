46a910b1613b3056236eabe6fb960f5a9a65e6ea
bintanxxx <sibintangadji@gmail.com>
2025-12-29 16:22:02 +0700

    feat(core): fixing port run localhost

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', config.SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=8080)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
